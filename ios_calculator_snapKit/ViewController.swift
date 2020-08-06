94a0c158c8e9b6ddad2c9de8d420766a5748eba0
ahmadrama0999 <ahmadrama0999@gmail.com>
2020-08-06 12:35:33 +0300

    Initial Commit

---
+++ ios_calculator_snapKit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios_calculator_snapKit
+//
+//  Created by Ramin Akhmad on 06.08.2020.
+//  Copyright © 2020 Ramin Akhmad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
